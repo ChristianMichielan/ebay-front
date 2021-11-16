72529b924fbe7e93b1eb3d1b32f1e893c43b5e12
Quentin <quentin.douris63@gmail.com>
2021-11-16 15:38:20 +0100

    segment dans la partie enchère

--- src/app/encheres/encheres.page.ts
+++ src/app/encheres/encheres.page.ts
@@ -7,6 +7,42 @@ import { Component } from '@angular/core';
 })
 export class EncheresPage {
 
+  bienUtilisateur = [];
+  segmentModel = 'en_cours';
+
+  /**
+   * Constructeur de la page
+   */
   constructor() {}
+
+  /**
+   * A l'initialisation de la page
+   */
+  ngOnInit() {
+
+  }
+
+  /**
+   * Evenement lors du changement d'onglet dans le segment
+   * Charge les annonces qui correspond au segment
+   * @param event
+   */
+  segmentChanged(event: any) {
+    switch (this.segmentModel) {
+      case 'en_cours':
+        console.log('Le segment a changé : ' + this.segmentModel);
+        break;
+      case 'livre':
+        console.log('Le segment a changé : ' + this.segmentModel);
+        break;
+      case 'vendu':
+        console.log('Le segment a changé : ' + this.segmentModel);
+        break;
+      default:
+        console.log('Le segment a changé : ' + this.segmentModel);
+        break;
+    }
+  }
+
 
 }
